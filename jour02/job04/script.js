6e09ac6faa695460e7f2ee04719d3159630aa2bb
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2023-01-17 14:22:11 +0100

    Runtrack-Jour02-Job04 => Création d'un <textarea> dont l'id est 'keylogger', à chaques fois qu'un user tape une lettre sur le clavier celle-ci est ajoutée, si le focus est dans <textarea> alors on ajoute deux fois la lettre

---
+++ jour02/job04/script.js
@@ -0,0 +1,23 @@
+// Créez un <textarea> dont l’id est “keylogger”.
+// Chaque fois que l’utilisateur tape une lettre sur son clavier (a-z), celle-ci est ajoutée
+// dans le textarea (même si le focus en cours n’est pas le textarea).
+// Si le focus en cours est dans le textarea, la lettre doit être ajoutée deux fois.
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let textarea = document.getElementById('keylogger');
+    let keylogger = '';
+
+    document.addEventListener('keydown', (e) => {
+        let key = e.key;
+        if (key.length === 1) {
+            keylogger += key;
+            if (document.activeElement === textarea) {
+                keylogger += key;
+            }
+            textarea.value = keylogger;
+        }
+    });
+
+
+});
